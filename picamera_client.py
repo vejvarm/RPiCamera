54b0ca5dc6beeb339ee85cae5f36f2171a78706a
Martin Vejvar <vejvarm@gmail.com>
2020-10-07 13:04:22 +0200

    chore: change client params

--- picamera_client.py
+++ picamera_client.py
@@ -13,11 +13,11 @@ client_socket.connect(('192.168.1.30', 8000))
 connection = client_socket.makefile('wb')
 try:
     camera = picamera.PiCamera()
-    camera.resolution = (320, 240)
-    camera.framerate = 24
+    camera.resolution = (640, 480)
+    camera.framerate = 12
     # Start a preview and let the camera warm up for 2 seconds
     camera.start_preview()
-    time.sleep(10)
+    time.sleep(5)
 
     # Note the start time and construct a stream to hold image data
     # temporarily (we could write it directly to connection but in this
